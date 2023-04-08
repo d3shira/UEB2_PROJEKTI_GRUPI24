cffb0e65404672169f4e8d4fa20872ac1e0bfa6f
daf1na <dafinabalaj345@gmail.com>
2023-04-08 14:55:35 +0200

    ..

--- navbar.js
+++ navbar.js
@@ -6,3 +6,17 @@ menu.onclick = () =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+var swiper = new Swiper(". home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
