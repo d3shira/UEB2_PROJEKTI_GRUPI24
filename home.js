447f1cf1ea399eed34722d9c5f173b0b87595c05
bl3rt4 <blerta.aazemi@gmail.com>
2023-04-16 20:14:06 +0300

    .

--- home.js
+++ home.js
@@ -13,3 +13,28 @@ var swiper = new Swiper(". home-slider", {
     loop:true,
   });
 
+/*Blerta -mduhet qetu me shtu si ne video ama ka edhe diqka nalt qesaj temes edhe posht qasaj qe koka garant qikat e shtojne*/
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    loop:true,
+    breakpoints:{
+   0:{
+   slidesPerView:1,
+   },
+   640:{
+   slidesPerView:2,
+   },
+   768:{
+   slidesPerView:2,
+   },
+   1024:{
+   slidesPerView:3,
+   },
+  },
+ });
