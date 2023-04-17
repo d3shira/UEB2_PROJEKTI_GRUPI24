3cdc2a11262ae4d413c3a5de69f9f4edfb7ad9c2
dafiinaa <dafinaasadiku@gmail.com>
2023-04-17 13:51:16 +0200

    .

--- home.js
+++ home.js
@@ -1,6 +1,26 @@
 
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+window.onscroll = () =>{
+  section.forEach(sec =>{
+
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop -150;
+    let id = sec.getAttribute('id');
+
+    if(top => offset && top < offset + height){
+      navLinks.forEach(links =>{
+        links.classList.remove('active');
+        document.querySelector('header .navbar a[href*='+id+']).classList.add('active);
+      });
+    };
+  });
+}
+
 var swiper = new Swiper(". home-slider", {
-    spaceBetween: 30,
+    spaceBetween: 20,
     centeredSlides: true,
     autoplay: {
       delay: 7500,
@@ -13,3 +33,24 @@ var swiper = new Swiper(". home-slider", {
     loop:true,
   });
 
+
+
+
+
+
+
+
+
+  function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out');
+  }
+
+  function fadeOut(){
+    setInterval(loader, 3000);
+  }
+  window.onload = fadeOut;
+
+  .loader-container.fade-out{
+    top:110%;
+    opacity:0;
+  }
