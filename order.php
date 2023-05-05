8f892bd603e745394bdf5df741d2cb40cce7d5c7
dafiinaa <dafinaasadiku@gmail.com>
2023-05-05 15:34:17 +0200

    .

---
+++ order.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+
+<body>
+    
+<!-- footer section starts -->
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>locations</h3>
+            <a href="#">india</a>
+            <a href="#">japan</a>
+            <a href="#">russia</a>
+            <a href="#">USA</a>
+            <a href="#">france</a>
+        </div>
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="#">home</a>
+            <a href="#">dishes</a>
+            <a href="#">about</a>
+            <a href="#">menu</a>
+            <a href="#">review</a>
+            <a href="#">order</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#">+383-123-123</a>
+            <a href="#">+383-111-222</a>
+            <a href="#">email1</a>
+            <a href="#">email2</a>
+            <a href="#">prishtine, kosove -10000</a>
+    </div>
+    <div class="box">
+        <h3>follow us</h3>
+        <a href="#">facebook</a>
+        <a href="#">twitter</a>
+        <a href="#">instagram</a>
+        <a href="#">linkedin</a>
+</div>
+
+<div class="credit">copyright @ 2021 by <span>mr. web designer</span></div>
+</section>
+
+<!-- footer section ends -->
+
+<!-- loader part -->
+<div class="loader-container">
+    <img src="images/loader.gif">
+</div>
+
+
+<!-- <--Order section starts--> -->
+<section class="order" id="order">
+
+    <h3 class="sub-heading">order now</h3>
+    <h1 class="heading">free and fast</h1>
+
+    <form action="">
+        <div class="inputBox">
+            <div class="input">
+                <span>your name</span>
+                <input type="text" placeholder="enter your name">
+            </div>
+            <div class="input">
+                <span>your number</span>
+                <input type="number" placeholder="enter your number">
+            </div>
+        </div>
+        <div class="inputBox">
+            <div class="input">
+                <span>your order</span>
+                <input type="text" placeholder="enter food name">
+            </div>
+            <div class="input">
+                <span>additional food</span>
+                <input type="test" placeholder="extra with food">
+            </div>
+        </div>
+        <div class="inputBox">
+            <div class="input">
+                <span>How much</span>
+                <input type="number" placeholder="how many orders">
+            </div>
+            <div class="input">
+                <span>date and time</span>
+                <input type="datetime-local">
+            </div>
+        </div>
+        <div class="inputBox">
+            <div class="input">
+                <span>your address</span>
+            <textarea name="" placeholder="enter your address" id="" cols="30" rows="10"></textarea>
+            </div>
+            <div class="input">
+                <span>your message</span>
+            <textarea name="" placeholder="enter you message" id="" cols="30" rows="10"></textarea>
+            </div>
+        </div>
+
+        <input type="submit" value="order now" class="btn">
+    </form>
+
+</section>
+
+<!-- <--Order section ends--> -->
+
+</body>
+</html>
