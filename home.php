52064467ab64ff9b1cdd8b1a00d809fc05f3ca83
daf1na <dafinabalaj345@gmail.com>
2023-05-06 15:27:57 +0200

    .

--- home.php
+++ home.php
@@ -7,7 +7,7 @@
     <title>Document</title>
     
     <script src="home.js"></script> 
-     <link rel="stylesheet" href="navbar.css">
+   
     <link rel="stylesheet" href="home.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
