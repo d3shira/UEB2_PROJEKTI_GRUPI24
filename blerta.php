22ff8cd2edd1fae68e519ad78e7bee362753635c
daf1na <dafinabalaj345@gmail.com>
2023-05-06 17:11:31 +0200

    order + navbari

--- blerta.php
+++ blerta.php
@@ -9,8 +9,9 @@
     <link rel="stylesheet" href="home.php">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css">
     <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="index.css">
+    
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
--- order.php
+++ order.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css">
     <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="index.css">
     
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
     <title>Document</title>
 </head>
 <body>
+<?php @include 'navbar.php' ?>
 <section class="order" id="order">
 
 <h3 class="sub-heading">order now</h3>
