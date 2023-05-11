7d0f24a768508a095ddc734b3610e5a0db0796d1
d3shira <shirarandobrava@live.com>
2023-05-11 14:13:40 +0200

    admin dashboard start

---
+++ navbar-admin.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Healthy Lifestyle Website</title>
+    
+
+    <!--font awesome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <!--css-->
+    <link rel="stylesheet" href="navbar.css">
+    <link rel="stylesheet" href="home.css">
+
+    <!--<script src="home.js"></script>--> 
+    <script src="navbar.js"></script> 
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
+ 
+
+</head>
+<body>
+    
+
+    <!--HEADER SECTION-->
+    <!--me nderru disa icons dhe menu-->
+    <header>
+    <a href="#" class="logo"><i class="fas fa-utensils"></i> FitYou - Admin Panel </a>
+    <nav class="navbar">
+        <a class="" href="">Manage Users</a>
+        <a class="" href="">Manage Diets</a>
+        <a class="" href="">Manage Orders</a>
+    </nav>
+    <div class="icons">
+        <i class="fas fa-bars" id="menu-bars"></i>
+        <a href="#" class="fa-solid fa-user"></a>
+    </div>
+</header>
+
+<script> 
+const navbarLinks = document.querySelectorAll('.navbar a');
+
+navbarLinks.forEach(navbarLink => {
+  navbarLink.addEventListener('click', () => {
+    navbarLinks.forEach(navbarLink => {
+      navbarLink.classList.remove('active');
+    });
+    navbarLink.classList.add('active');
+  });
+});
+
+</script>
+</body>
+</html>
