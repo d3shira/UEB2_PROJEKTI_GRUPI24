2d7dba90812ee758a768552acb5ac3e2c279be88
daf1na <dafinabalaj345@gmail.com>
2023-05-12 13:45:07 +0200

    Create clientdb.php

---
+++ client/clientdb.php
@@ -0,0 +1,18 @@
+<?php
+$dbhost='localhost:3307';
+$dbuser='root';
+
+$dbpass='';
+$db='ueb2'; 
+$conn=mysqli_connect($dbhost,$dbuser,$dbpass,$db);
+
+if(!$conn)
+{
+  die('Could not connect: '.mysqli_connect_error());
+  
+}
+
+echo 'Connected successfully<br>';
+
+
+?>
