82f6a8734b1211ce7212382ee88ae9201aa8ad31
dafiinaa <dafinaasadiku@gmail.com>
2023-05-12 18:47:24 +0200

    .

--- delete.php
+++ delete.php
@@ -1,3 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Delete questions</title>
+
+    <style>
+h1{
+    text-align:center;
+    color: #192a56;
+    }  
+
+    table {
+    border-collapse: collapse;
+    width: 100%;
+    border: 2px solid #ccc;
+  }
+
+  td {
+    padding: 10px;
+    border: 1px solid #ccc;
+  }
+
+  .center {
+    text-align: center;
+  }
+  
+  p{
+font-size: 20px;
+  }
+
+  .deletebtn{
+    background-color:#e95b5b;
+    width:100px;
+    height:50px;
+    font-size: 20px;
+    border-radius:10px;
+
+  }
+
+  .deletebtn:hover{
+background-color: grey;
+  }
+
+    </style>
+</head>
+
+
+
 <?php
 
 // Connect to the database
@@ -41,12 +89,43 @@ $faq = $statement->fetch();
 
 ?>
 
+
+
+
+ 
+
 <!-- Display the FAQ and delete form -->
-<h1>FAQ Details</h1>
-<p>Question: <?php echo $faq["question"]; ?></p>
+<!-- <h1>FAQ Details</h1>
+<div >
+<p >Question: <?php echo $faq["question"]; ?></p>
 <p>Answer: <?php echo $faq["answer"]; ?></p>
+</div>
 
 <form method="POST" action="" onsubmit="return confirm('Are you sure you want to delete this FAQ?');">
     <input type="hidden" name="id" value="<?php echo $faq['faq_id']; ?>" required>
     <input type="submit" name="submit" value="Delete">
-</form>
+</form> -->
+<h1>FAQ Details</h1>
+
+<table>
+  <tr>
+    <td>
+      
+      <div>
+        <p>Question: <?php echo $faq["question"]; ?></p>
+        <p>Answer: <?php echo $faq["answer"]; ?></p>
+      </div>
+    </td>
+    <td class="center">
+      <form method="POST" action="" onsubmit="return confirm('Are you sure you want to delete this FAQ?');">
+        <input type="hidden" name="id" value="<?php echo $faq['faq_id']; ?>" required>
+        <input  class=" deletebtn"type="submit" name="submit" value="Delete">
+      </form>
+    </td>
+  </tr>
+</table>
+
+
+
+
+</html>
