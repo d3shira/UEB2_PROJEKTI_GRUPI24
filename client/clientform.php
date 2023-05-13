03b3e198879d18853fcf5310139061b53875ed16
d3shira <shirarandobrava@live.com>
2023-05-13 02:42:59 +0200

    .

--- client/clientform.php
+++ client/clientform.php
@@ -1,35 +1,35 @@
-
-
+<?php require_once "../database.php";?>
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="admin/register-staff.css">
-    <title>Insert Client Profile</title>
+	<link rel="stylesheet" href="admin/register-staff.css">
+	<title>Insert Client Profile</title>
 </head>
 <body>
-    <h2>Insert Client Profile</h2>
-    <form action="insert_profile.php" method="post">
-        <div class="form-group">
-            <label>Birthday: </label>
-            <input type="date" name="birthday" required>
-        </div>
-        <div class="form-group">
-            <label>Weight (kg): </label>
-            <input type="number" name="weight" required>
-        </div>
-        <div class="form-group">
-            <label>Height (cm): </label>
-            <input type="number" name="height" required>
-        </div>
-        <div class="form-group">
-            <input type="submit" value="Submit">
-        </div>
-    </form>
+	<div class="wrapper">
+		<h2>Insert Client Profile</h2>
+		<form action="insert_profile.php" method="post">
+			<div class="form-group">
+				<label>Birthday: </label>
+				<input type="date" name="birthday" class="form-control" required>
+			</div>
+			<div class="form-group">
+				<label>Weight (kg): </label>
+				<input type="number" name="weight" class="form-control" required>
+			</div>
+			<div class="form-group">
+				<label>Height (cm): </label>
+				<input type="number" name="height" class="form-control" required>
+			</div>
+			<div class="form-group">
+				<input type="submit" value="Submit" class="btn btn-primary">
+			</div>
+		</form>
+	</div>
 </body>
 </html>
 
 <?php 
-require_once "../database.php";
 // Get the database connection
 
 
