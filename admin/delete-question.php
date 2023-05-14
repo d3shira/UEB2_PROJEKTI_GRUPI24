aabbaa8cc2f8a8da20e2d7efc132a787895f6175
dafiinaa <dafinaasadiku@gmail.com>
2023-05-14 16:07:49 +0200

    .

---
+++ admin/delete-question.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Delete questions</title>
+
+    <style>
+   
+h1{
+    text-align:center;
+    color: #192a56;
+    }  
+
+    table {
+        margin-left:550px;
+    border-collapse: collapse;
+    width: 30%;
+    border: 2px solid #ccc;
+  }
+
+  td {
+    padding: 10px;
+    border: 1px solid #ccc;
+  }
+
+  .center {
+    text-align: center;
+  }
+  
+  p{
+font-size: 20px;
+  }
+
+  .deletebtn{
+     background: #666;
+    width:100px;
+    height:50px;
+    font-size: 20px;
+    border-radius:10px;
+
+  }
+
+  .deletebtn:hover{
+    background: #534e4e;
+  }
+
+    </style>
+</head>
+
+
+
+<?php
+
+// Connect to the database
+$host = "localhost:3307";
+$dbname = "ueb2";
+$username = "root";
+$password = 'Replace.3';
+
+try {
+    // Create a new PDO connection
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Check if the delete form is submitted
+    if (isset($_POST["submit"])) {
+        // Delete the FAQ from the database
+        $sql = "DELETE FROM tbl_faq WHERE faq_id = ?";
+        $statement = $conn->prepare($sql);
+        $statement->execute([$_POST["id"]]);
+
+        // Redirect back to the previous page
+        header("Location: " . $_SERVER["HTTP_REFERER"]);
+        exit();
+    }
+
+    // Check if the FAQ exists
+   // Check if the FAQ exists
+$sql = "SELECT * FROM tbl_faq WHERE faq_id = ?";
+$statement = $conn->prepare($sql);
+$statement->execute([$_REQUEST["id"]]); // Change $_GET to $_REQUEST
+$faq = $statement->fetch();
+
+
+    if (!$faq) {
+        die("FAQ not found");
+    }
+
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
+
+
+
+
+ 
+
+<!-- Display the FAQ and delete form -->
+<!-- <h1>FAQ Details</h1>
+<div >
+<p >Question: <?php echo $faq["question"]; ?></p>
+<p>Answer: <?php echo $faq["answer"]; ?></p>
+</div>
+
+<form method="POST" action="" onsubmit="return confirm('Are you sure you want to delete this FAQ?');">
+    <input type="hidden" name="id" value="<?php echo $faq['faq_id']; ?>" required>
+    <input type="submit" name="submit" value="Delete">
+</form> -->
+<h1>FAQ Details</h1>
+
+<table>
+  <tr>
+    <td>
+      
+      <div>
+        <p>Question: <?php echo $faq["question"]; ?></p>
+        <p>Answer: <?php echo $faq["answer"]; ?></p>
+      </div>
+    </td>
+    <td class="center">
+      <form method="POST" action="" onsubmit="return confirm('Are you sure you want to delete this FAQ?');">
+        <input type="hidden" name="id" value="<?php echo $faq['faq_id']; ?>" required>
+        <input  class=" deletebtn"type="submit" name="submit" value="Delete">
+      </form>
+    </td>
+  </tr>
+</table>
+
+
+
+
+</html>
---
+++ admin/edit-question.php
@@ -0,0 +1,142 @@
+<?php
+// Connect to the database using PHPMyAdmin
+$servername = "localhost:3307";
+$username = "root";
+$password = "Replace.3";
+$dbname = "ueb2";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Check if FAQ exists
+    $sql = "SELECT * FROM tbl_faq WHERE faq_id = :id";
+    $statement = $conn->prepare($sql);
+    $statement->bindParam(':id', $_REQUEST["id"]);
+    $statement->execute();
+    $faq = $statement->fetch();
+
+    if (!$faq) {
+        die("FAQ not found");
+    }
+
+    // Update the FAQ in the database
+    if (isset($_POST["submit"])) {
+        $updateSql = "UPDATE tbl_faq SET question = :question, answer = :answer WHERE faq_id = :id";
+        $updateStatement = $conn->prepare($updateSql);
+        $updateStatement->bindParam(':question', $_POST["question"]);
+        $updateStatement->bindParam(':answer', $_POST["answer"]);
+        $updateStatement->bindParam(':id', $_POST["id"]);
+        $updateStatement->execute();
+
+        // Redirect to FAQs page or show a success message
+        header("Location: faqs.php");
+        exit();
+    }
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
+<style>
+
+    .text-center{
+        text-align:center;
+        color: #192a56;
+
+    }
+
+    .form-control{
+        height:50px;
+        width:200px;
+    }
+    .form-group {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        font-size:20px;
+        font-weight:600;
+        margin-left:550px;
+      }
+
+
+
+.form-group textarea {
+  padding: 10px;
+  resize: vertical;
+}
+
+#answer{
+    margin-top:5px;
+    margin-left:12px;
+}
+
+
+.btnSubmit{
+     margin-left:730px;
+     height:50px;
+     width:100px;
+     margin-top:10px;
+     font-weight:600;
+     font-size:16px;
+ background-color:#27ae60;
+    border-radius:8px;
+}
+
+.btnSubmit:hover{
+    background-color:#19914b;
+}
+</style>
+
+
+<!-- include CSS -->
+<link rel="stylesheet" href="add.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.css" />
+<link rel="stylesheet" type="text/css" href="richtext/richtext.min.css" />
+
+<!-- include JS -->
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="richtext/jquery.richtext.js"></script>
+
+
+
+<!-- layout for form to edit FAQ -->
+<div class="container" style="margin-top: 50px; margin-bottom: 50px;">
+    <div class="row">
+        <div class="offset-md-3 col-md-6">
+            <h1 class="text-center">Edit FAQ</h1>
+
+            <!-- form to edit FAQ -->
+            <form method="POST" action="edit.php">
+
+                <!-- hidden ID field of FAQ -->
+                <input type="hidden" name="id" value="<?php echo $faq['faq_id']; ?>" required />
+
+                <!-- question, auto-populate -->
+                <div class="form-group">
+                    <label>Enter Question</label>
+                    <input type="text" name="question" class="form-control" value="<?php echo $faq['question']; ?>" required />
+                </div>
+
+                <!-- answer, auto-populate -->
+                <div class="form-group">
+                    <label>Enter Answer</label>
+                    <textarea name="answer" id="answer" class="form-control" required><?php echo $faq['answer']; ?></textarea>
+                </div>
+
+
+                <!-- submit button -->
+               <div class="butoniSubmit">
+                <input  type="submit" name="submit" class="btnSubmit" value="Edit FAQ" /></div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script>
+    // initialize rich text library
+    window.addEventListener("load", function () {
+        $("#answer").richText();
+    })
+</script>
