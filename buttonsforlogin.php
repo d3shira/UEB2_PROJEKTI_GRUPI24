97e932af164e9bc4c35725e7cf720d74e4bf759e
daf1na <dafinabalaj345@gmail.com>
2023-05-15 14:30:54 +0200

    Update buttonsforlogin.php

--- buttonsforlogin.php
+++ buttonsforlogin.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Log In</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">-->
    
 </head>
 <body>
