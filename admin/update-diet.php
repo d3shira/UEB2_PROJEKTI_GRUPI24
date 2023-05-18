3cd1ab3c4f06372be9b1d6d09cf909402effaaa7
Besmira75 <besmira.berisha129@gmail.com>
2023-05-18 15:25:57 +0200

    diets

--- admin/update-diet.php
+++ admin/update-diet.php
@@ -2,17 +2,57 @@
 <?php
 require_once "../database.php";
 
-// Initialize the session
-session_start(); 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-// Check if the user is logged in, if not then redirect him to login page
-//if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true && ['user_type']!=='admin'){
-  //  header("location: ../login.php");
-    //exit;
-//}
+// Define the SQL query
+$sql2 = "INSERT INTO tbl_diet (diet_name, description, price, in_stock, image_path) VALUES (?, ?, ?, ?, ?)";
+
+// Check if the form is submitted
+if (isset($_POST['submit'])) {
+    // Process the form data
+    $diet_name = $_POST['diet_name'];
+    $description = $_POST['description'];
+    $price = $_POST['price'];
+    $in_stock = isset($_POST['in_stock']) ? $_POST['in_stock'] : "No";
+    $image_path = $_POST['image_path'];
+
+    // Check if any required fields are empty
+    $errors = array();
+    if (empty($diet_name)) {
+        $errors[] = "Diet Name is required";
+    }
+    if (empty($description)) {
+        $errors[] = "Description is required";
+    }
+    if (empty($price)) {
+        $errors[] = "Price is required";
+    }
+    if (empty($image_path)) {
+        $errors[] = "Image Path is required";
+    }
+
+    // If there are any errors, display them and stop execution
+    if (!empty($errors)) {
+        foreach ($errors as $error) {
+            echo $error . "<br>";
+        }
+        exit();
+    }
+
+    // Create a variable $stmt and assign the result of the prepared statement execution
+    $stmt = $conn->prepare($sql2);
+    $stmt->bind_param("sssss", $diet_name, $description, $price, $in_stock, $image_path);
+    $result = $stmt->execute();
+
+    // Redirect after processing the form
+    if ($result == true) {
+        header("Location: diet-success.php");
+        exit();
+    } else {
+        echo "Failed to Insert Diet";
+        exit();
+    }
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -35,10 +75,119 @@ error_reporting(E_ALL);
     <script src="../navbar.js"></script> 
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
+    <style>
+        /* CSS for .main-content */
+.main-content {
+  padding: 100px;
+}
 
+/* CSS for .wrapper */
+.wrapper {
+  max-width: 500px;
+  margin: 0 auto;
+  background-color: #f1f1f1;
+  padding: 20px;
+}
+
+/* CSS for .wrapper h1 */
+.wrapper h1 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+/* CSS for .wrapper table */
+.wrapper table {
+  width: 100%;
+  font-size: 18px;
+}
+
+/* CSS for .wrapper table td */
+.wrapper table td {
+  padding: 10px;
+}
+
+/* CSS for .wrapper input[type="text"], .wrapper input[type="number"], .wrapper textarea */
+.wrapper input[type="text"],
+.wrapper input[type="number"],
+.wrapper textarea {
+  width: 100%;
+  padding: 8px;
+  border: 1px solid #ccc;
+}
+
+/* CSS for .wrapper input[type="submit"] */
+.wrapper input[type="submit"] {
+  display: block;
+  margin: 0 auto;
+  background-color: #4caf50;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  cursor: pointer;
+}
+
+/* CSS for .wrapper input[type="submit"]:hover */
+.wrapper input[type="submit"]:hover {
+  background-color: #45a049;
+}
+
+/* CSS for .wrapper .success, .wrapper .error */
+.wrapper .success,
+.wrapper .error {
+  margin-top: 10px;
+  padding: 10px;
+}
+
+/* CSS for .wrapper .success */
+.wrapper .success {
+  background-color: #d4edda;
+  color: #155724;
+}
+
+/* CSS for .wrapper .error */
+.wrapper .error {
+  background-color: #f8d7da;
+  color: #721c24;
+}
+    </style>
 </head>
 <body>
-<?php @include "navbar-admin.php"?>
+<!--HEADER SECTION-->
+    <!--me nderru disa icons dhe menu-->
+       <!--HEADER SECTION-->
+    <!--me nderru disa icons dhe menu-->
+    <header style="text-decoration:none;">
+    <a href="admin dashboard.php" class="logo"><i class="fas fa-utensils"></i> FitYou - Admin Dashboard </a>
+    <nav class="navbar">
+        <div class="dropdown">
+            <a class="dropbtn">Manage Users</a>
+            <div class="dropdown-content">
+                <a href="admin-manage-staff.php">Manage Staff</a>
+                <a href="admin-manage-clients.php">Manage Clients</a>
+            </div>
+        </div>
+        <a class="" href="admin-manage-diets.php">Manage Diets</a>
+        <a class="" href="admin-manage-orders.php">Manage Orders</a>
+        <a href="admin-manage-questions.php">Manage Questions</a>
+    </nav>
+    <div class="icons">
+        <i class="fas fa-bars" id="menu-bars"></i>
+        <a href="admin dashboard.php" class="fa-solid fa-user"></a>
+    </div>
+</header>
+<script> 
+const navbarLinks = document.querySelectorAll('.navbar a');
+
+navbarLinks.forEach(navbarLink => {
+  navbarLink.addEventListener('click', () => {
+    navbarLinks.forEach(navbarLink => {
+      navbarLink.classList.remove('active');
+    });
+    navbarLink.classList.add('active');
+  });
+});
+
+</script>
 <?php 
     //CHeck whether id is set or not 
     if(isset($_GET['diet_id']))
@@ -96,7 +245,7 @@ error_reporting(E_ALL);
             <tr>
                 <td>Price: </td>
                 <td>
-                    <input type="number" name="price" value="<?php echo $price; ?>">
+                    <input type="decimal" name="price" value="<?php echo $price; ?>">
                 </td>
             </tr>
 
@@ -162,19 +311,7 @@ error_reporting(E_ALL);
                 $res3 = mysqli_query($conn, $sql3);
 
                 //CHeck whether the query is executed or not 
-                if($res3==true)
-                {
-                    //Query Exectued and Food Updated
-                    $_SESSION['update'] = "<div class='success'>Food Updated Successfully.</div>";
-                    header('location:http://localhost/UEB2_PROJEKTI_GRUPI24/admin/admin-manage-diets.php');
-                }
-                else
-                {
-                    //Failed to Update Food
-                    $_SESSION['update'] = "<div class='error'>Failed to Update Food.</div>";
-                    header('location:http://localhost/UEB2_PROJEKTI_GRUPI24/admin/admin-manage-diets.php');
-                }
-
+            
                 
             }
         
